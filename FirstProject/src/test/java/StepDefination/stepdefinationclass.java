9bab719acc4b323e17ae867fc4b15e67e5dbf274
SharmilaPau <sharmila.paul@maersk.com>
2022-04-27 13:05:02 +0530

    commit 3

---
+++ FirstProject/src/test/java/StepDefination/stepdefinationclass.java
@@ -0,0 +1,59 @@
+package StepDefination;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import hooks.hooksclass;
+import junit.framework.Assert;
+
+public class stepdefinationclass {
+	
+	
+	WebDriver driver = hooksclass.driver;
+	
+
+
+	@Given("User navigated to the Login page")
+	public void user_navigated_to_the_Login_page() {
+		
+		
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		
+	    System.out.println("user is navigated to the login page");
+	    System.out.println("user is navigated to the login page");
+	}
+
+	@Given("Enter Username {string} and password {string}")
+	public void enter_Username_and_password(String un, String pw) throws InterruptedException {
+		
+		driver.findElement(By.id("email")).sendKeys(un);;
+		
+		driver.findElement(By.id("pass")).sendKeys(pw);
+		
+		driver.findElement(By.name("login")).click();
+		Thread.sleep(5000);
+		
+		String Expected = "Enter an email or phone number";
+		String actual = driver.findElement(By.id("")).getText();
+		
+		Assert.assertEquals(Expected, actual);
+		
+		
+		
+		//System.out.println("my user name is "+username+" and my password is "+password);
+	    
+	}
+	
+	@Then("Error message displayed")
+	public void error_message_displayed() {
+	   System.out.println("error message");
+	}
+
+
+
+}
---
+++ FirstProject/src/test/java/features/Retail.feature
@@ -0,0 +1,23 @@
+Feature: Signin feature
+  I want to signin in my portal
+
+  @Regression @Smoke
+  Scenario Outline: Verify Facebook Login page
+    Given User navigated to the Login page
+    And Enter Username "<username>" and password "<password>"
+    Then Error message displayed
+
+    Examples:
+      | username          | password       |
+      | abc               | abcd1234       |
+
+
+  @tag1  @Regression
+  Scenario Outline: Verify Facebook Login page
+    Given User navigated to the Login page
+    When Enter Username "<username>" and password "<password>"
+    Then Error message displayed
+
+    Examples:
+      | username          | password       |
+      | abc               | abcd1234       |
