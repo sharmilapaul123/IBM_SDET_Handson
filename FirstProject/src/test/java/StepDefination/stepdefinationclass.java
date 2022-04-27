4ab56b9878f6b22842df2b67e797c3279b0f9b2b
SharmilaPau <sharmila.paul@maersk.com>
2022-04-27 13:54:13 +0530

    commit 4

--- FirstProject/src/test/java/StepDefination/stepdefinationclass.java
+++ FirstProject/src/test/java/StepDefination/stepdefinationclass.java
@@ -1,7 +1,9 @@
 package StepDefination;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.java.en.Given;
@@ -9,6 +11,8 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import hooks.hooksclass;
 import junit.framework.Assert;
+
+import java.util.UUID;
 
 public class stepdefinationclass {
 	
@@ -21,31 +25,22 @@ public class stepdefinationclass {
 	public void user_navigated_to_the_Login_page() {
 		
 		
-		driver.get("https://www.facebook.com/");
+		driver.get("http://retailm1.upskills.in/admin/index.php?route=common/dashboard&token=fOtazgcSDn3hST8dy4F1RPyPR15xQsnO");
 		driver.manage().window().maximize();
 		
-	    System.out.println("user is navigated to the login page");
 	    System.out.println("user is navigated to the login page");
 	}
 
 	@Given("Enter Username {string} and password {string}")
 	public void enter_Username_and_password(String un, String pw) throws InterruptedException {
-		
-		driver.findElement(By.id("email")).sendKeys(un);;
-		
-		driver.findElement(By.id("pass")).sendKeys(pw);
-		
-		driver.findElement(By.name("login")).click();
-		Thread.sleep(5000);
-		
-		String Expected = "Enter an email or phone number";
-		String actual = driver.findElement(By.id("")).getText();
-		
-		Assert.assertEquals(Expected, actual);
-		
-		
-		
-		//System.out.println("my user name is "+username+" and my password is "+password);
+		Thread.sleep(2000);
+		driver.findElement(By.id("input-username")).sendKeys(un);;
+		Thread.sleep(2000);
+		driver.findElement(By.id("input-password")).sendKeys(pw);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[contains(text(),' Login')]")).click();
+		Thread.sleep(2000);
+
 	    
 	}
 	
@@ -53,6 +48,36 @@ public class stepdefinationclass {
 	public void error_message_displayed() {
 	   System.out.println("error message");
 	}
+	@When("user is navigated successfully to the  Home page")
+	public void user_navigated_to_the_Homepage_page() {
+		if (driver.findElement(By.xpath("//h1[contains(text(),'Dashboard')]")).getText()=="Dashboard"){
+			System.out.println("user is navigated successfully to the  Home page");
+		}
+	}
+	@When("Created coupons")
+	public void couponCreation() throws InterruptedException {
+		driver.findElement(By.xpath("//*[@id='menu-marketing']/a")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id='menu-marketing']/ul/li[3]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@data-original-title='Add New']")).click();
+		String uniqueString = UUID.randomUUID().toString();
+		driver.findElement(By.id("input-name")).sendKeys(uniqueString);
+		driver.findElement(By.id("input-code")).sendKeys("1234");
+		WebElement element = driver.findElement(By.xpath("//*[@data-original-title='Save']"));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+		Thread.sleep(5000);
+
+
+
+
+
+	}
+
+
+
+
+
 
 
 
--- FirstProject/src/test/java/features/Retail.feature
+++ FirstProject/src/test/java/features/Retail.feature
@@ -1,23 +1,13 @@
 Feature: Signin feature
-  I want to signin in my portal
 
-  @Regression @Smoke
-  Scenario Outline: Verify Facebook Login page
+  @Regression
+  Scenario : Verify retail site Login page
+
     Given User navigated to the Login page
-    And Enter Username "<username>" and password "<password>"
-    Then Error message displayed
-
-    Examples:
-      | username          | password       |
-      | abc               | abcd1234       |
+    And Enter Username "admin" and password "Admin@123"
+    When user is navigated successfully to the  Home page
+    And Created coupons
 
 
-  @tag1  @Regression
-  Scenario Outline: Verify Facebook Login page
-    Given User navigated to the Login page
-    When Enter Username "<username>" and password "<password>"
-    Then Error message displayed
 
-    Examples:
-      | username          | password       |
-      | abc               | abcd1234       |
+
